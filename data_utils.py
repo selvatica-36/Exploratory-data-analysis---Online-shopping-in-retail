4eb1fa0996a0f4774569a0fe83d3139d32bd068d
selvatica-36 <silvia.h.aragon@gmail.com>
2023-11-18 22:13:05 +0000

    Adds 3 methods to class Plotter.

--- data_utils.py
+++ data_utils.py
@@ -1,85 +1,12 @@
-import pandas as pd
+from scipy.stats import chi2_contingency
+import missingno as msno
 import numpy as np
-
-class DataTransform:
-        def __init__(self, dataframe):
-            self.df = dataframe.copy()
-        
-        # TODO: add error handling
-        # TODO: add remaining data types if applicable
-        def convert_to_type(self, column_name, data_type, ignore_errors = True):
-                data_type = data_type.lower()
-                if ignore_errors == True:
-                    try:
-                        if data_type == 'datetime':
-                            self.df[column_name] = pd.to_datetime(self.df[column_name], errors = 'coerce')
-                        elif data_type == 'date':
-                            self.df[column_name] = pd.to_datetime(self.df[column_name], errors = 'coerce')
-                        elif data_type == 'categorical':
-                            self.df[column_name] = pd.Categorical(self.df[column_name])
-                        elif data_type == 'str':
-                            self.df[column_name] = self.df[column_name].astype(str, errors='ignore')
-                        elif data_type == 'float' or data_type == 'float64':
-                            self.df[column_name] = self.df[column_name].astype(float, errors='ignore')
-                        elif data_type == 'int' or data_type == 'int64':
-                            self.df[column_name] = self.df[column_name].astype(int, errors='ignore')
-                        elif data_type == 'bool':
-                            self.df[column_name] = self.df[column_name].astype(bool, errors='ignore')
-                        else:
-                            print(f"Error: data type {data_type} not supported. Check docstrings or call help for more information.")
-                    except Exception as e:
-                        print(f"Error converting column '{column_name}' to type '{data_type}': {e}")
-                    return self.df.copy()
-                elif ignore_errors == False:
-                    try:
-                        if data_type == 'datetime':
-                            self.df[column_name] = pd.to_datetime(self.df[column_name])
-                        elif data_type == 'date':
-                            self.df[column_name] = pd.to_datetime(self.df[column_name])
-                        elif data_type == 'timedelta':
-                            self.df[column_name] = pd.to_timedelta(self.df[column_name])
-                        elif data_type == 'categorical':
-                            self.df[column_name] = pd.Categorical(self.df[column_name])
-                        elif data_type == 'str':
-                            self.df[column_name] = self.df[column_name].astype(str)
-                        elif data_type == 'float' or data_type == 'float64':
-                            self.df[column_name] = self.df[column_name].astype(float)
-                        elif data_type == 'int' or data_type == 'int64':
-                            self.df[column_name] = self.df[column_name].astype(int)
-                        elif data_type == 'bool':
-                            self.df[column_name] = self.df[column_name].astype(bool)
-                        else:
-                            print(f"Error converting column '{column_name}' to type '{data_type}': {e}")
-                    except Exception as e:
-                        print(f"Error converting column '{column_name}' to type '{data_type}': {e}")
-                    return self.df.copy()
-                else:
-                    print("Error: the parameter 'ignore_errors' is a bool and can only be True or False.")
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.style as style
+import seaborn as sns
 
 
-        def convert_month_to_period(self, column_name):
-            try:
-                self.df[column_name] = self.df[column_name].astype(str)
-                self.df['month'] = self.df['month'].str.lower()
-                month_map = {'jan': 1, 'feb': 2, 'mar': 3, 'apr': 4, 'may': 5, 'june': 6,
-                            'jul': 7, 'aug': 8, 'sep': 9, 'oct': 10, 'nov': 11, 'dec': 12}
-                self.df[column_name] = self.df[column_name].map(month_map)
-                self.df[column_name] = pd.to_datetime(self.df[column_name], format='%m', errors='coerce').dt.to_period('M')
-            except Exception as e:
-                print(f"Error converting 'month' column to period: {e}")
-            return self.df.copy()
-
-        def convert_columns(self, column_list, data_type, ignore_errors=True):
-            for column in column_list:
-                self.convert_to_type(column, data_type, ignore_errors)
-            return self.df.copy()
-        
-        def impute_nulls(self):
-            pass
-
-    
- 
- # TODO: add error handling
 class DataFrameInfo:
     def __init__(self, dataframe):
         self.df = dataframe.copy()
@@ -144,27 +71,111 @@ class DataFrameInfo:
             'null_percentage': null_percentages
         })
         return null_info
+    def chi_square_test(self, column_1, column_2):
+        new_df_test = self.df.copy()
+        new_df_test[column_1] = new_df_test[column_1].isnull()
+        # Step 2: Crosstab the new column with B
+        contingency_table = pd.crosstab(new_df_test[column_1], new_df_test[column_2])
+        # Step 3: Perform chi-squared test
+        chi2, p, dof, expected = chi2_contingency(contingency_table)
+        print(f"Chi-square statistic = {chi2}")
+        print(f"p-value = {p}")
+        
+
+class DataTransform:
+        def __init__(self, dataframe):
+            self.df = dataframe.copy()
+
+        def convert_to_type(self, column_name, data_type, ignore_errors = True):
+                data_type = data_type.lower()
+                if ignore_errors == True:
+                    try:
+                        if data_type == 'datetime':
+                            self.df[column_name] = pd.to_datetime(self.df[column_name], errors = 'coerce')
+                        elif data_type == 'date':
+                            self.df[column_name] = pd.to_datetime(self.df[column_name], errors = 'coerce')
+                        elif data_type == 'categorical':
+                            self.df[column_name] = pd.Categorical(self.df[column_name])
+                        elif data_type == 'str':
+                            self.df[column_name] = self.df[column_name].astype(str, errors='ignore')
+                        elif data_type == 'float' or data_type == 'float64':
+                            self.df[column_name] = self.df[column_name].astype(float, errors='ignore')
+                        elif data_type == 'int' or data_type == 'int64':
+                            self.df[column_name] = self.df[column_name].astype(int, errors='ignore')
+                        elif data_type == 'bool':
+                            self.df[column_name] = self.df[column_name].astype(bool, errors='ignore')
+                        else:
+                            print(f"Error: data type {data_type} not supported. Check docstrings or call help for more information.")
+                    except Exception as e:
+                        print(f"Error converting column '{column_name}' to type '{data_type}': {e}")
+                    return self.df.copy()
+                elif ignore_errors == False:
+                    try:
+                        if data_type == 'datetime':
+                            self.df[column_name] = pd.to_datetime(self.df[column_name])
+                        elif data_type == 'date':
+                            self.df[column_name] = pd.to_datetime(self.df[column_name])
+                        elif data_type == 'timedelta':
+                            self.df[column_name] = pd.to_timedelta(self.df[column_name])
+                        elif data_type == 'categorical':
+                            self.df[column_name] = pd.Categorical(self.df[column_name])
+                        elif data_type == 'str':
+                            self.df[column_name] = self.df[column_name].astype(str)
+                        elif data_type == 'float' or data_type == 'float64':
+                            self.df[column_name] = self.df[column_name].astype(float)
+                        elif data_type == 'int' or data_type == 'int64':
+                            self.df[column_name] = self.df[column_name].astype(int)
+                        elif data_type == 'bool':
+                            self.df[column_name] = self.df[column_name].astype(bool)
+                        else:
+                            print(f"Error converting column '{column_name}' to type '{data_type}': {e}")
+                    except Exception as e:
+                        print(f"Error converting column '{column_name}' to type '{data_type}': {e}")
+                    return self.df.copy()
+                else:
+                    print("Error: the parameter 'ignore_errors' is a bool and can only be True or False.")
+
+        def convert_month_to_period(self, column_name):
+            try:
+                self.df[column_name] = self.df[column_name].astype(str)
+                self.df['month'] = self.df['month'].str.lower()
+                month_map = {'jan': 1, 'feb': 2, 'mar': 3, 'apr': 4, 'may': 5, 'june': 6,
+                            'jul': 7, 'aug': 8, 'sep': 9, 'oct': 10, 'nov': 11, 'dec': 12}
+                self.df[column_name] = self.df[column_name].map(month_map)
+                self.df[column_name] = pd.to_datetime(self.df[column_name], format='%m', errors='coerce').dt.to_period('M')
+            except Exception as e:
+                print(f"Error converting 'month' column to period: {e}")
+            return self.df.copy()
+
+        def convert_columns(self, column_list, data_type, ignore_errors=True):
+            for column in column_list:
+                self.convert_to_type(column, data_type, ignore_errors)
+            return self.df.copy()
+        
+        def impute_nulls_with_median(self, columns_list):
+                for column in columns_list:
+                    print("Use this method if NaN values are likely MCAR (p > 0.05)")
+                    self.df[column] = self.df[column].fillna(self.df[column].median())
+                return self.df
+        def impute_nulls_with_mode(self, columns_list):
+                for column in columns_list:
+                    print("Use this method if NaN values are likely MCAR (p > 0.05)")
+                    self.df[column] = self.df[column].fillna(self.df[column].mode()[0])
+                return self.df
+            
 
 
-
-class Plotter:
+class Plotter():
     def __init__(self,dataframe):
         self.df = dataframe.copy()
+        
     def discrete_probability_distribution(self, column_name):
         plt.rc("axes.spines", top=False, right=False)
         sns.set_style(style='darkgrid', rc=None)
-        style.use('fivethirtyeight')
-        five_thirty_eight = [
-            "#30a2da",
-            "#fc4f30",
-            "#e5ae38",
-            "#6d904f",
-            "#8b8b8b",
-        ]
-        sns.set_palette(five_thirty_eight)
         probs = self.df[column_name].value_counts(normalize=True)
         # Create bar plot
-        dpd = sns.barplot(y=probs.values, x=probs.index,palette=five_thirty_eight)
+        dpd = sns.barplot(y=probs.values, x=probs.index)
+        dpd.set_xticklabels(dpd.get_xticklabels(), rotation=45, ha='right')
         plt.xlabel('Values')
         plt.ylabel('Probability')
         plt.title('Discrete Probability Distribution')
@@ -172,6 +183,20 @@ class Plotter:
         print(f"The mode of the distribution is {self.df[column_name].mode()[0]}")
         print(f"The mean of the distribution is {self.df[column_name].mean()}")
         print(f"The median of the distribution is {self.df[column_name].median()}")
+
+    def continuous_probability_distribution(self, column_name, column_list=None):
+        if column_list is not None:
+            for col in column_list:
+                sns.histplot(self.df[col], kde=True, color='blue', stat="probability", bins=30)
+                print(f"The mode of the distribution is {self.df[col].mode()[0]}")
+                print(f"The mean of the distribution is {self.df[col].mean()}")
+                print(f"The median of the distribution is {self.df[col].median()}")
+        else:
+            sns.histplot(self.df[column_name], kde=True, color='blue', stat="probability", bins=30)
+            print(f"The mode of the distribution is {self.df[column_name].mode()[0]}")
+            print(f"The mean of the distribution is {self.df[column_name].mean()}")
+            print(f"The median of the distribution is {self.df[column_name].median()}")
+        
 
     def correlation_heatmap(self, column_list):
         sns.heatmap(self.df[column_list].corr(), annot=True, cmap='coolwarm')
